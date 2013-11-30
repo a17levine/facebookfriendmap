d6ec4dfd014a2e65e4205d84a1b4e82454a5726c
Alex Levine <a17levine@gmail.com>
2013-11-30 02:35:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facebookfriendmap::Application.config.secret_key_base = '1543165476ed96bb5e83caa4d458796e21740ddc202511b69a11eb8bf61643ce2aa33e60dfe744a9f59f9510ef234b252337f5f5a32a717efa4f53089f1abcbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Facebookfriendmap::Application.config.session_store :cookie_store, key: '_facebookfriendmap_session'
